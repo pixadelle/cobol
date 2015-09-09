2184cd856ea99a3fec34a9bfebf521e91f254818
Emily <pixadelle@gmail.com>
2015-09-08 19:40:50 -0600

    inital commit

---
+++ new_world.cob
@@ -0,0 +1,20 @@
+      ******************************************************************
+      * Author: Emily
+      * Date: April 18, 2015
+      * Purpose: Trying a new IDE
+      * Tectonics: cobc
+      ******************************************************************
+       IDENTIFICATION DIVISION.
+      *-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-
+       PROGRAM-ID. BRAVE-NEW-WORLD.
+      *-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-
+       PROCEDURE DIVISION.
+      *-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-
+       MAIN-PROCEDURE.
+      **
+      * The main procedure of the program
+      **
+            DISPLAY "Woo! It's a whole new world!".
+            STOP RUN.
+
+       END PROGRAM BRAVE-NEW-WORLD.
